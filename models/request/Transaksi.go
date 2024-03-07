3210c45e2cd21ec977feb527deae53060a0112d2
Kendrew18 <kendrewtandio71@gmail.com>
2024-03-07 09:56:11 +0700

    commit 07-03-2024

--- models/request/Transaksi.go
+++ models/request/Transaksi.go
@@ -2,7 +2,7 @@ package request
 
 type Input_Transaksi_Request struct {
 	Co                    int     `json:"co"`
-	Kode_transaksi        string  `json:"kode_pembayaran"`
+	Kode_transaksi        string  `json:"kode_transaksi"`
 	Kode_nota             string  `json:"kode_nota"`
 	Tanggal               string  `json:"tanggal"`
 	Kode_jenis_pembayaran string  `json:"kode_jenis_pembayaran"`
@@ -14,17 +14,17 @@ type Input_Transaksi_Request struct {
 }
 
 type Input_Barang_Transaksi_Request struct {
-	Co                              int    `json:"co"`
-	Kode_transaksi                  string `json:"kode_transaksi"`
-	Kode_inventory                  string `json:"kode_inventory"`
-	Kode_barang_transaksi_inventory string `json:"kode_barang_transaksi_inventory"`
-	Jumlah_barang                   int    `json:"jumlah_barang"`
-	Nama_satuan                     string `json:"nama_satuan"`
-	Harga                           int64  `json:"harga"`
-	Sub_total                       int64  `json:"sub_total"`
+	Co                    int     `json:"co"`
+	Kode_barang_transaksi string  `json:"kode_barang_transaksi"`
+	Kode_transaksi        string  `json:"kode_transaksi"`
+	Kode_inventory        string  `json:"kode_inventory"`
+	Jumlah_barang         float64 `json:"jumlah_barang"`
+	Nama_satuan           string  `json:"nama_satuan"`
+	Harga                 int64   `json:"harga"`
+	Sub_total             int64   `json:"sub_total"`
 }
 
 type Body_Input_Transaksi_Request struct {
-	Input_transaksi        Input_Transaksi_Request        `json:"input_transaksi"`
-	Input_barang_transaksi Input_Barang_Transaksi_Request `json:"input_barang_transaksi"`
+	Input_transaksi        Input_Transaksi_Request          `json:"input_transaksi"`
+	Input_barang_transaksi []Input_Barang_Transaksi_Request `json:"input_barang_transaksi"`
 }
--- service/transaksi/Transaksi.go
+++ service/transaksi/Transaksi.go
@@ -5,6 +5,7 @@ import (
 	"Bakend-POS/models/request"
 	"Bakend-POS/models/response"
 	"Bakend-POS/tools/session_checking"
+	"math"
 	"net/http"
 	"strconv"
 	"time"
@@ -35,7 +36,7 @@ func Input_Transaksi(Request request.Body_Input_Transaksi_Request) (response.Res
 		}
 
 		Request.Co = co + 1
-		Request.Kode_pembayaran = "TR-" + strconv.Itoa(Request.Co)
+		Request.Kode_transaksi = "TR-" + strconv.Itoa(Request.Co)
 
 		date, _ := time.Parse("02-01-2006", Request.Tanggal)
 		Request.Tanggal = date.Format("2006-01-02")
@@ -57,7 +58,7 @@ func Input_Transaksi(Request request.Body_Input_Transaksi_Request) (response.Res
 		Request.Kode_nota = Request.Kode_nota + strconv.Itoa(co_pembayaran)
 		Request.Jumlah_total = 0.0
 
-		err = con.Table("transaksi").Select("co", "kode_pembayaran", "kode_nota", "tanggal", "kode_jenis_pembayaran", "kode_store", "jumlah_total", "total_harga", "kode_kasir", "diskon").Create(&Request)
+		err = con.Table("transaksi").Select("co", "kode_transaksi", "kode_nota", "tanggal", "kode_jenis_pembayaran", "jumlah_total", "total_harga", "diskon", "kode_user").Create(&Request)
 
 		if err.Error != nil {
 			res.Status = http.StatusNotFound
@@ -65,7 +66,63 @@ func Input_Transaksi(Request request.Body_Input_Transaksi_Request) (response.Res
 			res.Data = Request
 			return res, err.Error
 		}
+
+		co = 0
+
+		err = con.Table("barang_transaksi").Select("co").Limit(1).Order("co DESC").Scan(&co)
+
+		if err.Error != nil {
+			res.Status = http.StatusNotFound
+			res.Message = "Status Not Found"
+			res.Data = Request
+			return res, err.Error
+		}
+
+		total_harga := int64(0)
+		total_jumlah := 0.0
+
+		for i := 0; i < len(Request_barang); i++ {
+			Request_barang[i].Co = co + 1 + i
+			Request_barang[i].Kode_barang_transaksi = "BT-" + strconv.Itoa(Request_barang[i].Co)
+			Request_barang[i].Kode_transaksi = Request.Kode_transaksi
+			Request_barang[i].Sub_total = int64(math.Round(float64(Request_barang[i].Harga) * float64(Request_barang[i].Jumlah_barang)))
+			total_harga = total_harga + Request_barang[i].Sub_total
+
+			total_jumlah = total_jumlah + float64(Request_barang[i].Jumlah_barang)
+		}
+
+		total_harga = total_harga - Request.Diskon
+
+		err = con.Table("barang_transaksi").Select("co", "kode_barang_transaksi", "kode_transaksi", "kode_inventory", "jumlah_barang", "harga", "sub_total").Create(&Request_barang)
+
+		if err.Error != nil {
+			res.Status = http.StatusNotFound
+			res.Message = "Status Not Found"
+			res.Data = Request
+			return res, err.Error
+		}
+
+		err = con.Table("transaksi").Where("kode_transaksi = ?", Request.Kode_transaksi).Update("total_harga", total_harga).Update("total_barang", total_jumlah)
+
+		if err.Error != nil {
+			res.Status = http.StatusNotFound
+			res.Message = "Status Not Found"
+			res.Data = Request
+			return res, err.Error
+		} else {
+			res.Status = http.StatusOK
+			res.Message = "Suksess"
+			res.Data = map[string]int64{
+				"rows": err.RowsAffected,
+			}
+		}
 	}
+
+	return res, nil
+}
+
+func Read_Pembayaran() (response.Response, error) {
+	var res response.Response
 
 	return res, nil
 }
