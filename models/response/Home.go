6f648fef6fb56039c7502fd73c0667ba7456094b
Kendrew18 <kendrewtandio71@gmail.com>
2024-03-22 01:07:42 +0700

    commit check 15

--- models/response/Home.go
+++ models/response/Home.go
@@ -1,18 +1,18 @@
 package response
 
 type Home_Response struct {
-	Total_pemasukan          string              `json:"total_pemasukan"`
-	Total_pengeluaran        string              `json:"total_pengeluaran"`
-	Total_pembayaran_pending string              `json:"total_pembayaran_pending"`
+	Total_pemasukan          int64               `json:"total_pemasukan"`
+	Total_pengeluaran        int64               `json:"total_pengeluaran"`
+	Total_pembayaran_pending int64               `json:"total_pembayaran_pending"`
 	Chart_Pemasukan          []Chart_Pemasukan   `json:"chart_pemasukan" gorm:"-"`
 	Chart_Pengeluaran        []Chart_Pengeluaran `json:"chart_pengeluaran" gorm:"-"`
 }
 type Chart_Pengeluaran struct {
 	Tanggal           string `json:"tanggal"`
-	Total_pengeluaran string `json:"total_pengeluaran"`
+	Total_pengeluaran int64  `json:"total_pengeluaran"`
 }
 
 type Chart_Pemasukan struct {
 	Tanggal         string `json:"tanggal"`
-	Total_pemasukan string `json:"total_pemasukan"`
+	Total_pemasukan int64  `json:"total_pemasukan"`
 }
