f13a66a122b1f2b2f54fe45ac3c4bbc7760f5e9b
Kendrew18 <100029692+Kendrew18@users.noreply.github.com>
2024-03-31 14:58:47 +0700

    commit 31-03-2024 pt1

--- controllers/news/News.go
+++ controllers/news/News.go
@@ -5,59 +5,62 @@ import (
 	"Bakend-POS/models/response"
 	"Bakend-POS/service/news"
 	"Bakend-POS/tools/session_checking"
-	"encoding/json"
 	"fmt"
-	"log"
 	"net/http"
+	"strings"
 
 	"github.com/labstack/echo/v4"
 )
 
 func InputNews(c echo.Context) error {
-	var Request request.Input_News_Request
-	var Request_session request.Token_Request
+	//var Request request.Input_News_Request
+	//var Request_session request.Token_Request
 	var result response.Response
-	var err error
+	//var err error
 
 	data := c.FormValue("data")
 
 	fmt.Println(data)
 
-	jsonData := []byte(data)
+	str := strings.Split(data, "\n")
 
-	fmt.Println(jsonData)
+	fmt.Println(str)
 
-	err = json.Unmarshal(jsonData, &Request)
-	if err != nil {
-		log.Fatal(err)
-	}
+	// jsonData := []byte(data)
 
-	Request_session.Token = c.Request().Header.Get("token")
+	// fmt.Println(jsonData)
 
-	fmt.Println(Request)
+	// err = json.Unmarshal(jsonData, &Request)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
 
-	//err = c.Bind(&Request)
+	// Request_session.Token = c.Request().Header.Get("token")
 
-	if err != nil {
-		return c.JSON(http.StatusInternalServerError, map[string]string{"message": err.Error()})
-	}
+	// fmt.Println(Request)
 
-	User, condition := session_checking.Session_Checking(Request_session.Token)
+	// //err = c.Bind(&Request)
 
-	Request.Kode_user = User.Kode_user
+	// if err != nil {
+	// 	return c.JSON(http.StatusInternalServerError, map[string]string{"message": err.Error()})
+	// }
 
-	if condition {
+	// User, condition := session_checking.Session_Checking(Request_session.Token)
 
-		result, err = news.Input_News(Request, c.Response(), c.Request())
+	// Request.Kode_user = User.Kode_user
 
-		if err != nil {
-			return c.JSON(http.StatusInternalServerError, map[string]string{"message": err.Error()})
-		}
-	} else {
-		result.Status = http.StatusNotFound
-		result.Message = "Session Invalid"
-		result.Data = Request
-	}
+	// if condition {
+
+	// 	result, err = news.Input_News(Request, c.Response(), c.Request())
+
+	// 	if err != nil {
+	// 		return c.JSON(http.StatusInternalServerError, map[string]string{"message": err.Error()})
+	// 	}
+	// } else {
+	// 	result.Status = http.StatusNotFound
+	// 	result.Message = "Session Invalid"
+	// 	result.Data = Request
+	// }
 
 	return c.JSON(result.Status, result)
 }
