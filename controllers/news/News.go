b31614a6d85a31b70caad838d00206e908efd38a
Kendrew18 <kendrewtandio71@gmail.com>
2024-04-01 10:28:28 +0700

    commit 01-04-2024 pt1

--- controllers/news/News.go
+++ controllers/news/News.go
@@ -5,7 +5,9 @@ import (
 	"Bakend-POS/models/response"
 	"Bakend-POS/service/news"
 	"Bakend-POS/tools/session_checking"
+	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 	"strings"
 
@@ -13,7 +15,7 @@ import (
 )
 
 func InputNews(c echo.Context) error {
-	//var Request request.Input_News_Request
+	var Request request.Input_News_Request
 	//var Request_session request.Token_Request
 	var result response.Response
 	//var err error
@@ -34,11 +36,11 @@ func InputNews(c echo.Context) error {
 
 	fmt.Println(jsonData)
 
-	// err := json.Unmarshal(jsonData, &Request)
+	err := json.Unmarshal(jsonData, &Request)
 
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	// Request_session.Token = c.Request().Header.Get("token")
 
